06f2b64151aa5f5f4f512c785509f6da7b73016a
kanmi-idris <60736533+kanmi-idris@users.noreply.github.com>
2022-05-28 18:08:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+
+    def show(self):
+        print('The student name is', self.name, ',', 'he is', self.age, 'years old,',
+            'his courses are: ', self.tracks, 'and his overall score is', self.score,)
+
+    # To change student name
+    def change_name(self, new_name):
+        self.name = str(new_name)
+
+    # To change student age
+    def change_age(self, new_age):
+        self.age = int(new_age)
+
+    # To add new track to the existing one
+    def add_track(self, new_tracks):
+        self.tracks.append(new_tracks)
+
+    # To change student score
+    def get_score(self):
+        return self.score
+
+
+Bob = Student(name="Bob", age=26, tracks=["FullStack", "Backend"], score=50)
+
+# Expected methods
+Bob.change_name("Olasunkanmi")
+Bob.change_age(20)
+Bob.add_track("UI/UX")
+Bob.get_score()
+Bob.show()
